1c0e19d72157efe7cfe539f4931ccd7229cee454
Sagar Dagdu <shags032@gmail.com>
2021-06-04 15:17:24 +0530

    Initial code commit

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Sagar Dagdu on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
