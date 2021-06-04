6134a28befbacfaaa10e4a03534d4360f1c46271
Sagar Dagdu <shags032@gmail.com>
2021-06-04 15:25:39 +0530

    Add initial view for barcode scanner

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,33 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Sagar Dagdu on 04/06/21.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                Spacer().frame(height: 60)
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder").font(.title)
+                Text("Not Yet Scanned")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+            .preferredColorScheme(.dark)
+    }
+}
